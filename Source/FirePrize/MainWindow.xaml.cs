5626a644b987046831ba65048b7c41373b43dec4
David Rieman <drieman@weareratio.com>
2015-07-12 20:18:22 -0700

    FirePrize: Functioning FireBase usage against scratch area, and started the Prize Pool UI.

--- Source/FirePrize/MainWindow.xaml.cs
+++ Source/FirePrize/MainWindow.xaml.cs
@@ -1,27 +1,40 @@
-﻿using System;
+﻿using FireSharp;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FirePrize
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    /// <summary>Interaction logic for MainWindow.xaml</summary>
     public partial class MainWindow : Window
     {
+        private IFirebaseClient firebase;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Using FirePrize_Scratch DB; You must create your own Firebase DB (free), and use it here.
+            // TODO: Allow non-code reconfiguration, even for ClickOnce launchers.
+            var config = new FirebaseConfig()
+            {
+                AuthSecret = "m9hRXQvwANuKWJZJEbYWypPmsJw6MVCVRvfL8R80",
+                BasePath = "https://fiery-fire-2370.firebaseio.com/",
+            };
+            firebase = new FirebaseClient(config);
+        }
+
+        private async void NewPrizePool_Click(object sender, RoutedEventArgs e)
+        {
+            string name = this.newPrizePoolName.Text;
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                await firebase.PushAsync("prizePools", new PrizePool(name));
+            }
         }
     }
 }
---
+++ Source/FirePrize/PrizePool.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirePrize
+{
+    public class PrizePool
+    {
+        public PrizePool(string name)
+        {
+            this.Name = name;
+            this.Prizes = new List<Prize>();
+        }
+
+        public string Name { get; set; }
+        public List<Prize> Prizes { get; set; }
+    }
+}
