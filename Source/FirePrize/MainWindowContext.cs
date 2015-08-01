6a424db21e3f598c89f9d047d92a72bfa4eb7e44
David Rieman <davidrieman@users.noreply.github.com>
2015-08-01 16:17:09 -0700

    Missed these files.

---
+++ Source/FirePrize/MainWindowContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirePrize
+{
+    public class MainWindowContext : PropertyObservable
+    {
+        private FireCollection<PrizePool> prizePools;
+        public FireCollection<PrizePool> PrizePools
+        {
+            get { return prizePools; }
+            set { SetProperty<FireCollection<PrizePool>>(ref prizePools, value); }
+        }
+
+        private PrizePool selectedPrizePool;
+        public PrizePool SelectedPrizePool
+        {
+            get { return selectedPrizePool; }
+            set { SetProperty<PrizePool>(ref selectedPrizePool, value); }
+        }
+    }
+}
